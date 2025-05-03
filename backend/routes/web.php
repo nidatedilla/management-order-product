4d30c75cf8265a3dbebe7ae8a8a44cddcb2c9e31
nidatedilla <nidatedilla@gmail.com>
2025-05-03 13:07:57 +0700

    feat(backend): add PUT /products/{id} endpoint

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,3 +19,4 @@ $router->get('/', function () use ($router) {
 
 $router->get('/products', 'ProductController@index');
 $router->post('/products', 'ProductController@store');
+$router->put('/products/{id}', 'ProductController@update');
